295d9e875ca65ad94f0f95694bcb94518b14cb13
aysenurkurt22 <aysenurkurt@gmail.com>
2024-06-28 20:18:22 +0300

    Fix database connection to use environment variables

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+let connection;
+
+if (process.env.DB_URL) {
+  connection = mysql.createConnection(process.env.DB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+}
 
 connection.connect(err => {
   if (err) {
